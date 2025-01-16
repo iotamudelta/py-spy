bce039ce6fc4d8331fa82c9c0aa5c5a640cce833
Johannes M Dieterich <johannes.dieterich@amd.com>
2025-01-16 11:42:07 -0600

    debug

--- src/native_stack_trace.rs
+++ src/native_stack_trace.rs
@@ -306,9 +306,10 @@ impl NativeStack {
     }
 
     fn get_thread(&mut self, thread: &remoteprocess::Thread) -> Result<Vec<u64>, Error> {
+        println!("In get_thread");
         let mut stack = Vec::new();
         for ip in self.unwinder.cursor(thread)? {
-            println!("IP {}", ip);
+            println!("IP {:?}", ip);
             stack.push(ip?);
         }
         Ok(stack)
